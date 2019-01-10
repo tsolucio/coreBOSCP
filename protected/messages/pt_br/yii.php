3855663495d6c0a89f33c3e6e46e89bbe2ec891d
Joe Bordes <joe@tsolucio.com>
2019-01-10 19:41:28 +0100

    i18n(pt_br)

---
+++ protected/messages/pt_br/yii.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Message translations.
+ */
+return CMap::mergeArray(
+  require(Yii::getFrameworkPath().'/messages/pt/yii.php'),
+  array(
+  '__empty' => 'traducido',
+  )
+);
---
+++ protected/messages/pt_br/zii.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Message translations.
+ */
+return CMap::mergeArray(
+  require(Yii::getFrameworkPath().'/messages/pt/zii.php'),
+  array(
+  '__empty' => 'traducido',
+  )
+);
